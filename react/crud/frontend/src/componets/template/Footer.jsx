5099755c28bab369312428c21e778a19f98061d8
ltrismegistus <trismegistolucas1@gmail.com>
2020-09-18 21:58:17 -0300

    rodape

--- react/crud/frontend/src/componets/template/Footer.jsx
+++ react/crud/frontend/src/componets/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react'
 
 export default props => 
     <footer className="footer">
-        Rodape
+        <span>
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Lk</strong>
+        </span>
     </footer>
